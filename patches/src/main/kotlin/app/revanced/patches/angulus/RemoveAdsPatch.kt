a3d567ed3bdda878ce17a22b6492a20a38390c98
Denis Kerner <denis.kerner@vodafone.com>
2025-03-17 20:58:10 +0100

    refactor: improve formatting and import structure in RemoveAdsPatch.kt

--- patches/src/main/kotlin/app/revanced/patches/angulus/RemoveAdsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/angulus/RemoveAdsPatch.kt
@@ -1,18 +1,19 @@
 package app.revanced.patches.angulus
 
-import app.revanced.patcher.extensions.InstructionExtensions.returnEarly
 import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.util.returnEarly
 
 @Suppress("unused")
-val angulusPatch = bytecodePatch(
-    name = "Angulus Ad removal patch",
-    description = "This is a patch to remove the Ads from Angulus. Unlimited measurements.",
-) {
-    compatibleWith("com.drinkplusplus.angulus"("5"))
+val angulusPatch =
+        bytecodePatch(
+                name = "Angulus Ad removal patch",
+                description =
+                        "This is a patch to remove the Ads from Angulus. Unlimited measurements.",
+        ) {
+            compatibleWith("com.drinkplusplus.angulus"("5"))
 
-    execute {
-        // Remove the ads
-        angulusAdsFingerprint.method.returnEarly()
-    }
-
-}
+            execute {
+                // Remove the ads
+                angulusAdsFingerprint.method.returnEarly()
+            }
+        }
