ee0bfb8657e100ec7fe633b28265de857bf1e0d7
Denis Kerner <denis.kerner@vodafone.com>
2025-03-17 18:37:56 +0100

    refactor: clean up commented code in Fingerprints.kt

--- patches/src/main/kotlin/app/revanced/patches/angulus/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/angulus/Fingerprints.kt
@@ -3,13 +3,13 @@ package app.revanced.patches.angulus
 import com.android.tools.smali.dexlib2.AccessFlags
 import app.revanced.patcher.fingerprint
 
+//dailyMeasurementCount
+//lastMeasurementDate
+//dailyAdResetCount
+//MeasurementPrefs
+
 internal val angulusAdsFingerprint = fingerprint {
     accessFlags(AccessFlags.PRIVATE)
     returns("I")
     strings("dailyMeasurementCount")
 }
-
-//dailyMeasurementCount
-//lastMeasurementDate
-//dailyAdResetCount
-//MeasurementPrefs
