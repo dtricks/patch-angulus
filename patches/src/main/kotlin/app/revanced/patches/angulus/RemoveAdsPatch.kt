8580979a34dd4ed60adfbc08621c49526f208eca
Denis Kerner <denis.kerner@vodafone.com>
2025-03-17 19:57:36 +0100

    refactor: rename examplePatch to angulusPatch and update compatibility version

--- patches/src/main/kotlin/app/revanced/patches/angulus/RemoveAdsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/angulus/RemoveAdsPatch.kt
@@ -1,14 +1,14 @@
 package app.revanced.patches.angulus
 
-import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.patch.bytecodePatch
 
 @Suppress("unused")
-val examplePatch = bytecodePatch(
+val angulusPatch = bytecodePatch(
     name = "Angulus Ad removal patch",
-    description = "This is a patch to remove the Ads from Angulus.",
+    description = "This is a patch to remove the Ads from Angulus. Unlimited measurements.",
 ) {
-    compatibleWith("com.drinkplusplus.angulus"("5.0.20"))
+    compatibleWith("com.drinkplusplus.angulus"("5"))
 
     execute {
         // Remove the ads
